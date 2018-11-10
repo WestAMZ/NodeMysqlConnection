33ec600dc6670c3c76bcc883f7ade44febfb1f26
Westly Meza <westlymeza@gmail.com>
2018-11-09 22:39:41 -0600

    Ejemplo Terminado

--- src/app/routes/news.js
+++ src/app/routes/news.js
@@ -1,7 +1,36 @@
+const dbConnection = require('../../config/dbConnection');
  module.exports = (app) =>
  {
-     app.get('/',(req,res)=>
-     {
-         res.send('hello world');
-     });
+    const connection = dbConnection() 
+    app.get('/',(req,res)=>
+    {
+        connection.query('SELECT * FROM news',(err,result)=>
+            {
+                if(err)
+                {
+                    console.log(err);
+                }
+                else
+                {
+                    //console.log(result);
+                    res.render("news/news",{news:result});
+                }
+            });
+    });
+    app.post('/news',(req,res)=>
+    {
+        
+        const {title,news} = req.body;
+        connection.query('INSERT INTO news SET?',{title,news},(err,result)=>
+        {
+            res.redirect('/');
+        });
+    });
+    /*
+    Imprime en consola el objeto que se envia
+    console.log(req.body);
+    dado que atributos pasados a set se llaman igual se podria dejar
+    {title,news} en lugar de {title:valor,news:valor}
+    NOTA: no poner nombre de constantes identicos a campos de tabla (title:title) no funciona
+    */
  }
--- src/sql/database.sql
+++ src/sql/database.sql
@@ -1,6 +1,6 @@
-CREATE DATABASE news_portal;
+CREATE DATABASE news_portal_nodejs;
 
-USE news_portal;
+USE news_portal_nodejs;
 CREATE TABLE news(
     id_news INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
     title VARCHAR(100),
@@ -8,6 +8,6 @@ CREATE TABLE news(
     data_created TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
-DESCRIBE news;
+DESCRIBE news_nodejs;
 INSERT INTO news(title,news) VALUES('my title','content of the news');
 SELECT * FROM news;
